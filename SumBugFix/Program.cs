08cbf9fecee4144af9f7f1a2c2255ea492651869
Rebeca <rebeca.nonato.lemaf@gmail.com>
2020-10-07 12:08:55 -0300

    Add project files.

---
+++ SumBugFix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SumBugFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Average(2, 1));
+        }
+
+        private static double Average(double v1, double v2)
+        {
+            return (v1 + v2) / 2;
+        }
+    }
+}
